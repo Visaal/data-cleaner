fe98bebb1de0b42eb69a5f1c2686624212275f14
Visaal Patel <visaal.patel@gmail.com>
2020-05-11 20:31:45 +0100

    html for file input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <router-link to="/DataCleaner">Data Cleaner</router-link>
+  <div class="form-container">
+    <div class="file-selector">
+      <fieldset>
+        <label for="fileItem">Please select your file</label>
+        <input id="fileItem" type="file" />
+        <div>
+          <router-link to="/DataCleaner">Data Cleaner</router-link>
+        </div>
+      </fieldset>
+    </div>
   </div>
 </template>
 
@@ -11,4 +19,37 @@ export default {
 </script>
 
 <style>
+.form-container {
+  width: 20%;
+  height: 100%;
+  display: table;
+  margin: auto;
+}
+
+.file-selector {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+input[type="file"],
+select {
+  display: inline-block;
+  font-family: menlo;
+  height: 30px;
+  line-height: 30px;
+  color: var(--paragraph);
+  background-color: var(--field-grey);
+  border: none;
+  font-size: 0.9rem;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  box-sizing: border-box;
+  vertical-align: top;
+  outline: 0;
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 8px;
+}
 </style>
