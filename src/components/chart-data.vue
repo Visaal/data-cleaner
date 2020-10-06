0c8bd03e813d21be5596bcd029dc7a84555c29d3
Visaal Patel <visaal.patel@gmail.com>
2020-10-06 18:11:56 +0100

    basic styling for chart form

--- src/components/chart-data.vue
+++ src/components/chart-data.vue
@@ -30,89 +30,99 @@
       :style="positionStyle"
       ref="chartOptionBox"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="48"
-        height="48"
-        class="chart-option-button"
-      >
-        <path fill="none" d="M0 0H24V24H0z" />
-        <path
-          d="M5 3v16h16v2H3V3h2zm15.293 3.293l1.414 1.414L16 13.414l-3-2.999-4.293 4.292-1.414-1.414L13 7.586l3 2.999 4.293-4.292z"
-        />
-      </svg>
+      <div class="chart-icons">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="48"
+          height="48"
+          class="chart-option-button"
+        >
+          <path fill="none" d="M0 0H24V24H0z" />
+          <path
+            d="M5 3v16h16v2H3V3h2zm15.293 3.293l1.414 1.414L16 13.414l-3-2.999-4.293 4.292-1.414-1.414L13 7.586l3 2.999 4.293-4.292z"
+          />
+        </svg>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="48"
-        height="48"
-        class="chart-option-button"
-      >
-        <path fill="none" d="M0 0h24v24H0z" />
-        <path
-          d="M11 .543c.33-.029.663-.043 1-.043C18.351.5 23.5 5.649 23.5 12c0 .337-.014.67-.043 1h-1.506c-.502 5.053-4.766 9-9.951 9-5.523 0-10-4.477-10-10 0-5.185 3.947-9.449 9-9.95V.542zM11 13V4.062A8.001 8.001 0 0 0 12 20a8.001 8.001 0 0 0 7.938-7H11zm10.448-2A9.503 9.503 0 0 0 13 2.552V11h8.448z"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="48"
+          height="48"
+          class="chart-option-button"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M11 .543c.33-.029.663-.043 1-.043C18.351.5 23.5 5.649 23.5 12c0 .337-.014.67-.043 1h-1.506c-.502 5.053-4.766 9-9.951 9-5.523 0-10-4.477-10-10 0-5.185 3.947-9.449 9-9.95V.542zM11 13V4.062A8.001 8.001 0 0 0 12 20a8.001 8.001 0 0 0 7.938-7H11zm10.448-2A9.503 9.503 0 0 0 13 2.552V11h8.448z"
+          />
+        </svg>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="48"
-        height="48"
-        class="chart-option-button"
-      >
-        <path fill="none" d="M0 0h24v24H0z" />
-        <path
-          d="M2 13h6v8H2v-8zm14-5h6v13h-6V8zM9 3h6v18H9V3zM4 15v4h2v-4H4zm7-10v14h2V5h-2zm7 5v9h2v-9h-2z"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="48"
+          height="48"
+          class="chart-option-button"
+        >
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path
+            d="M2 13h6v8H2v-8zm14-5h6v13h-6V8zM9 3h6v18H9V3zM4 15v4h2v-4H4zm7-10v14h2V5h-2zm7 5v9h2v-9h-2z"
+          />
+        </svg>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="48"
-        height="48"
-        class="chart-option-button"
-      >
-        <path fill="none" d="M0 0L24 0 24 24 0 24z" />
-        <path
-          d="M16 16c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zM6 12c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm10 6c-.552 0-1 .448-1 1s.448 1 1 1 1-.448 1-1-.448-1-1-1zM6 14c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2zm8.5-12C17.538 2 20 4.462 20 7.5S17.538 13 14.5 13 9 10.538 9 7.5 11.462 2 14.5 2zm0 2C12.567 4 11 5.567 11 7.5s1.567 3.5 3.5 3.5S18 9.433 18 7.5 16.433 4 14.5 4z"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="48"
+          height="48"
+          class="chart-option-button"
+        >
+          <path fill="none" d="M0 0L24 0 24 24 0 24z" />
+          <path
+            d="M16 16c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zM6 12c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm10 6c-.552 0-1 .448-1 1s.448 1 1 1 1-.448 1-1-.448-1-1-1zM6 14c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2zm8.5-12C17.538 2 20 4.462 20 7.5S17.538 13 14.5 13 9 10.538 9 7.5 11.462 2 14.5 2zm0 2C12.567 4 11 5.567 11 7.5s1.567 3.5 3.5 3.5S18 9.433 18 7.5 16.433 4 14.5 4z"
+          />
+        </svg>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="48"
-        height="48"
-        class="chart-option-button"
-      >
-        <path fill="none" d="M0 0H24V24H0z" />
-        <path
-          d="M11 2.05v3.02C7.608 5.557 5 8.475 5 12c0 3.866 3.134 7 7 7 1.572 0 3.024-.518 4.192-1.394l2.137 2.137C16.605 21.153 14.4 22 12 22 6.477 22 2 17.523 2 12c0-5.185 3.947-9.449 9-9.95zM21.95 13c-.2 2.011-.994 3.847-2.207 5.328l-2.137-2.136c.687-.916 1.153-2.006 1.323-3.192h3.022zM13.002 2.05c4.724.469 8.48 4.226 8.95 8.95h-3.022c-.438-3.065-2.863-5.49-5.928-5.929V2.049z"
-        />
-      </svg>
-      <div>
-        <label for="fieldName">Select x-axis</label>
-        <select v-model="xFieldName">
-          <option v-for="field in dataSelectedFieldNames" :key="field">
-            {{ field }}
-          </option>
-        </select>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="48"
+          height="48"
+          class="chart-option-button"
+        >
+          <path fill="none" d="M0 0H24V24H0z" />
+          <path
+            d="M11 2.05v3.02C7.608 5.557 5 8.475 5 12c0 3.866 3.134 7 7 7 1.572 0 3.024-.518 4.192-1.394l2.137 2.137C16.605 21.153 14.4 22 12 22 6.477 22 2 17.523 2 12c0-5.185 3.947-9.449 9-9.95zM21.95 13c-.2 2.011-.994 3.847-2.207 5.328l-2.137-2.136c.687-.916 1.153-2.006 1.323-3.192h3.022zM13.002 2.05c4.724.469 8.48 4.226 8.95 8.95h-3.022c-.438-3.065-2.863-5.49-5.928-5.929V2.049z"
+          />
+        </svg>
       </div>
-      <div>
-        <label for="fieldName">Select y-axis</label>
-        <select v-model="yFieldName">
-          <option v-for="field in dataSelectedFieldNames" :key="field">
-            {{ field }}
-          </option>
-        </select>
-      </div>
-      <button @click="generateChart()">
-        Chart Data
-      </button>
+
+      <fieldset>
+        <div class="chart-option-container">
+          <div class="chart-option-section">
+            <label for="fieldName">Select x-axis</label>
+            <select v-model="xFieldName">
+              <option v-for="field in dataSelectedFieldNames" :key="field">
+                {{ field }}
+              </option>
+            </select>
+          </div>
+          <div class="chart-option-section">
+            <label for="fieldName">Select y-axis</label>
+            <select v-model="yFieldName">
+              <option v-for="field in dataSelectedFieldNames" :key="field">
+                {{ field }}
+              </option>
+            </select>
+          </div>
+          <div class="chart-option-action">
+            <button @click="generateChart()">
+              Chart Data
+            </button>
+          </div>
+        </div>
+      </fieldset>
+
       <div v-if="showChart">
         <column-chart :data="columnData" :options="chartOptions" />
       </div>
@@ -183,19 +193,17 @@ export default {
           title: this.chartTitle,
         },
         yAxis: {
-          title: "Temperature (Celsius)",
+          title: this.yFieldName,
           pointOnColumn: true,
         },
         xAxis: {
-          title: "Years",
+          title: this.xFieldName,
         },
         series: {
           showDot: true,
           zoomable: true,
         },
-        tooltip: {
-          suffix: "°C",
-        },
+        tooltip: {},
       };
     },
     generateChart() {
@@ -338,5 +346,35 @@ export default {
   -ms-user-select: none;
   user-select: none;
   position: relative;
+}
+
+.chart-icons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+
+.chart-option-section {
+  display: inline-block;
+}
+
+.chart-option-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 0px;
+}
+
+.chart-option-container .chart-option-section {
+  margin-right: 1%;
+}
+.chart-option-container .chart-option-section {
+  width: 44%;
+}
+
+.chart-option-container .chart-option-action {
+  /* width: 10%; */
 }
 </style>
